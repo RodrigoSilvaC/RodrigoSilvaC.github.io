390e6ce07496d4b2b94aa421be106d9e1cda78fb
RodrigoSilvaC <rod.silva.x@hotmail.com>
2022-10-15 16:20:41 -0400

    Updated Contact me section

---
+++ SendEmail.php
@@ -0,0 +1,15 @@
+<?php
+ if(isset($_POST['Send Message']))
+{
+    $to = "rodrigo.silva.casarrubias@gmail.com"; // this is your Email address
+    $from = $_POST['Email']; // this is the sender's Email address
+    $first_name = $_POST['Name'];
+    $subject = "Form submission";
+    $message = $first_name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", I will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
